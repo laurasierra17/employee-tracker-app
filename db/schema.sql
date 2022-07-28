0d1cf6ef500fc261067bc0b86ed9ba899d33a2b6
laurasierra17 <laura.sierra17@gmail.com>
2022-07-27 20:21:30 -0400

    create table department

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,10 @@
 DROP DATABASE IF EXISTS business_db;
 CREATE DATABASE business_db;
 USE business_db;
+
+-- Table that holds all of the departments
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+)
+

f19992b81fde3d8db3c3a063b7dc5738df938bcb
laurasierra17 <laura.sierra17@gmail.com>
2022-07-27 20:52:38 -0400

    create table employee

--- db/schema.sql
+++ db/schema.sql
@@ -19,3 +19,18 @@ CREATE TABLE role (
     REFERENCES department(id)
     ON DELETE SET NULL
 );
+
+-- Table with information about an employee
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
+);

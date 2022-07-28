bcc7dae5bcfec74617825d3bb9fcfc3d99dd12a1
laurasierra17 <laura.sierra17@gmail.com>
2022-07-27 21:03:04 -0400

    populate department table

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+-- Data to pre-populate the database, to make the development of individual features much easier
+
+-- Add all of the departments
+INSERT INTO department (id, name)
+VALUES
+    (1, "Engineering"),
+    (2, "Finance"),
+    (3, "Legal"),
+    (4, "Sales"),
+    (5, "Service");

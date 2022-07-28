6a1b323ed69ec249f76293f2a047cd8c30afbb3c
laurasierra17 <laura.sierra17@gmail.com>
2022-07-28 11:59:36 -0400

    set up initial inquirer prompt

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const inquirer = require('inquirer');
-// Import and require mysql2
-const mysql = require('mysql2');
 const Database = require('./config/business_db');
 
 // Instantiate database
 const mydb = new Database();
-mydb.addEmployee("Laura", "Sierra", 4, 6);
-function init() {
 
+function init() {
+    inquirer.prompt(
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit"],
+            name: "action"
+        }
+    ).then(response => {
+        console.log(response);
+    })
 }
 
 // Commence mysql and iquirer as soon as the project runs

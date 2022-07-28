4d23c483dbe415835d0aea1eb7a57cd4dd70f331
laurasierra17 <laura.sierra17@gmail.com>
2022-07-27 21:11:32 -0400

    populate role table

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,3 +8,14 @@ VALUES
     (3, "Legal"),
     (4, "Sales"),
     (5, "Service");
+
+INSERT INTO role (id, title, salary, department_id)
+VALUES
+    (1, "Sales Lead", 100000, 4),
+    (2, "Salesperson", 80000, 4),
+    (3, "Lead Engineer", 150000, 1),
+    (4, "Software Engineer", 120000, 1),
+    (5, "Account Manager", 160000, 2),
+    (6, "Accountant", 125000, 2),
+    (7, "Legal Team Lead", 250000, 3),
+    (8, "Lawyer", 190000, 3);
